4cf6f93371d7a49c6552617d2719e25529fb9b10
t0xicwise <46230448+t0xicwise@users.noreply.github.com>
2018-12-28 16:32:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -515,4 +515,47 @@ const prefix = '*'
     }
   });
 
+client.on("message", message => {
+    const prefix = "*"
+              
+          if(!message.channel.guild) return;
+   if(message.author.bot) return;
+      if(message.content === prefix + "image"){ 
+          const embed = new Discord.RichEmbed()
+  
+      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
+  .setAuthor(message.author.username, message.guild.iconrURL)
+    .setColor(0x164fe3)
+    .setImage(message.guild.iconURL)
+    .setURL(message.guild.iconrURL)
+                    .setTimestamp()
+
+   message.channel.send({embed});
+      }
+  });
+
+client.on('message', message => {
+    if (message.content.startsWith("*avatar")) {  //// الامر و البرفيكس
+        if (message.author.bot) return
+        var mentionned = message.mentions.users.first();
+    var omar;
+      if(mentionned){
+          var omar = mentionned;
+      } else {
+          var omar = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setAuthor('Avatar Link :')
+        .setTitle('Click Here')
+        .setURL(`${omar.avatarURL}`)
+        .setImage(`${omar.avatarURL}`)
+        .setFooter('KilS CommunitY ',client.user.avatarURL) 
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
