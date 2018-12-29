1a7dd90bac0209c5a29dbb9e3eb8c7765556035e
t0xicwise <46230448+t0xicwise@users.noreply.github.com>
2018-12-28 16:11:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -215,4 +215,27 @@ client.on('ready', () => {//new ready event
   }, 5000);//the rainbow time
 })
 
+client.on("message", message => {
+var prefix = "%" // البريفكس
+    var args = message.content.substring(prefix.length).split(" ");
+    if (message.content.startsWith(prefix + "مسح")) { // الامر
+        if(!message.channel.guild) return message.reply('**❌ اسف لكن هذا الامر للسيرفرات فقط **');         
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**⚠  لا يوجد لديك صلاحية لمسح الشات**');
+var msg;
+msg = parseInt();
+
+message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+message.channel.sendMessage("", {embed: {
+title: "``تــم مسح الشات ``",
+color: 0x06DF00,
+footer: {
+  
+}
+}}).then(msg => {msg.delete(3000)});
+                  }
+
+
+});
+
+
 client.login(process.env.BOT_TOKEN);
